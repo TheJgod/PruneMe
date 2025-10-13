576cc92bfba2128eabb110164be496d539ac9983
samyuktsriram <93282154+samyuktsriram@users.noreply.github.com>
2025-10-13 11:26:54 +0200

    Update run_layer_similarity.sh

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -4,10 +4,10 @@
 
 # Run the Python script with command-line arguments
 python layer_similarity.py --model_path "mistralai/Mistral-7B-Instruct-v0.2" \
-                      --dataset "arcee-ai/sec-data-mini" \
-                      --dataset_column "text" \
-                      --batch_size 8 \
+                      --dataset "openai/gsm8k" \
+                      --dataset_column "question" \
+                      --batch_size 16 \
                       --max_length 1024 \
                       --layers_to_skip 8 \
-                      --dataset_size 4000 \
+                      --dataset_size 2000 \
                       --dataset_subset "train" 
