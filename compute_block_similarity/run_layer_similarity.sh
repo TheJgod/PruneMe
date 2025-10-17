e7e77a9d61b803d416414f27aeef1f3fb435e546
Samyukt Sriram <93282154+samyuktsriram@users.noreply.github.com>
2025-10-17 16:31:39 +0200

    Update run_layer_similarity.sh

--- compute_block_similarity/run_layer_similarity.sh
+++ compute_block_similarity/run_layer_similarity.sh
@@ -3,11 +3,11 @@
 # This Bash script runs the Python script with arguments
 
 # Run the Python script with command-line arguments
-python layer_similarity.py --model_path "mistralai/Mistral-7B-Instruct-v0.2" \
+python layer_similarity.py --model_path "unsloth/gpt-oss-20b-unsloth-bnb-4bit" \
                       --dataset "openai/gsm8k" \
                       --dataset_column "question" \
-                      --batch_size 16 \
+                      --batch_size 4 \
                       --max_length 1024 \
-                      --layers_to_skip 8 \
+                      --layers_to_skip 4 \
                       --dataset_size 2000 \
                       --dataset_subset "train" 
